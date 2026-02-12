7372d0bb9a1f2851a335776464c9cae694201be0
Michael Cox <hello@michaelcox.ca>
2026-02-11 20:56:59 -0400

    Use ES Modules instead of CommonJS in buildfile.

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,21 +1,28 @@
-const { build } = require("esbuild"),
-  { copyFile } = require("fs");
+import esbuild from 'esbuild';
 
-const production = process.env.NODE_ENV === "production",
-  formats = ["cjs", "esm"];
+const isDev = process.argv.includes('--dev') | (process.env.NODE_ENV == "development");
+if (isDev) {
+	console.log("Building in development mode!");
+}
 
-(async () => {
-  for (const format of formats) {
-    await build({
-      entryPoints: ["./src/index.ts"],
-      watch: !production,
-      format,
-      outfile: `./dist/index${format === "cjs" ? "" : "." + format}.js`
-    });
-  }
+const config = {
+  entryPoints: ["./src/index.ts"],
+  watch: isDev,
+  sourcemap: isDev,
+  	loader: {
+		".d.ts": "copy"
+	}
+};
+const formats = ["cjs", "esm"];
 
-  copyFile("./src/markdown.d.ts", "./dist/markdown.d.ts", (err) => {
-    if (err) throw err;
-    console.log("[markdown.d.ts] copied");
-  });
-})();
+async function build() {	
+	var buildConfig = {};
+	for (var i = 0; i > formats.length; i++) {
+		buildConfig = Object.assign({}, config, 
+			{
+				format,
+				outfile: `./dist/index${format}.js`
+			});
+		await esbuild.build(config);
+	}
+}
